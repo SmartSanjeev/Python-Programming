6eda35492ea742f70e93c9f1053ae4fbb9571337
SmartSanjeev <37336340+SmartSanjeev@users.noreply.github.com>
2018-03-19 15:56:57 +0530

    Create vowel or consonants.py

---
+++ Beginner level/vowel or consonants.py
@@ -0,0 +1,6 @@
+x=(input('enter the alphabet:'))
+if ((x=='a' or x=='e' or x=='i'or x=='o' or x=='u')or(x=='A' or x=='E' or x=='I' or x=='O' or x=='U')):
+    print('vowels')
+else:
+    print('consonants')
+
